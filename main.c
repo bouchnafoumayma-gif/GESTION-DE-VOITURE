f6f7f5138e90314b9c9b2e92e209dc9203d7fc3c
oumayma <bouchnafomayma546@gmail.com>
2025-12-01 14:11:30 +0100

    finission de fonction employe

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include "employe.h"
 
+int main() {
+    char login[30];
+    char motdepasse[30];
+    int result;
 
-int main(){
-	
- return 0;	
+    afficherCadre("DONNER VOTRE IDENTIFIANT !");
+    scanf("%s", login);
+
+    afficherCadre("DONNER VOTRE MOT DE PASSE");
+    scanf("%s", motdepasse);
+
+    result = oumayma(login, motdepasse);
+
+    if (result == 1) {
+        afficherCadre("Connexion reussie en tant qu'EMPLOYE !");
+    }
+    else if (result == 2) {
+        afficherCadre("Connexion reussie en tant qu'ADMIN !");
+        gestionDesEmployee();
+    }
+    else {
+        afficherCadre("ID OU MOT DE PASSE INCORRECT !");
+    }
+
+    return 0;
 }
+
